e0d86e3c07691a618fdcdf2f466a00bb2d85bb6c
Charlie Root <root@mothership.fbs.com>
2019-11-09 22:30:35 +0200

    wip

--- build.sh
+++ build.sh
@@ -17,10 +17,3 @@ export GOPATH="${OD}"
 export GOBIN="${OD}"
 go get
 go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/bs_router"
-
-# build and store objects into original directory.
-#go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/tile38-server" cmd/tile38-server/*.go
-#go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/tile38-cli" cmd/tile38-cli/*.go
-#go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/tile38-benchmark" cmd/tile38-benchmark/*.go
-#go build -ldflags "$LDFLAGS -extldflags '-static'" -o "$OD/tile38-luamemtest" cmd/tile38-luamemtest/*.go
-
